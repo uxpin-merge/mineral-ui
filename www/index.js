7acdebf54c0a842592ecda4ae385e2c0f163ee27
Mike Holm <mholm@rallydev.com>
2017-10-31 13:00:33 -0600

    WIP: rerender all the triangles from the original svg

--- www/index.js
+++ www/index.js
@@ -1,4 +1,22 @@
 const fs = require('fs');
+const triangles = require('./ripped_gems');
+
+function mesh(num, width, height) {
+  const verticies = [],
+        area = Math.sqrt(width * height / num / 2);
+  console.log(area);
+
+  for (let i=0, row=1; i<num; row++) {
+    for (let offset=0; offset<width; i++, offset+=area) {
+      const x = offset + Math.random() * area,
+            y = row * Math.random() * area,
+            v = `${x} ${y}`;
+      console.log(v);
+      verticies[i] = [x, y];
+    }
+  }
+  return verticies;
+}
 
 function spreadAttrs(attrs) {
   return Object.keys(attrs).map(key => {
@@ -19,16 +37,6 @@ function tag(name) {
   };
 }
 
-const path = tag("path");
-const animate = tag("animate");
-const svg = tag("svg").bind(this, {
-  id:"gem",
-  xmlns:"http://www.w3.org/2000/svg",
-  "xmlns:xlink":"http://www.w3.org/1999/xlink",
-  width:"300",
-  height:"200"
-});
-
 function animateColors(...colors) {
   return animate({
     attributeName: "fill",
@@ -39,37 +47,46 @@ function animateColors(...colors) {
   });
 }
 
+function getDimensions(triangles) {
+  return triangles.reduce(({xmin, ymin, xmax, ymax}, {one, two, three}) => {
+    const triangleXs = [one, two, three].map(p => p.x);
+    const triangleYs = [one, two, three].map(p => p.y);
+    return {
+      xmin: Math.min(xmin, ...triangleXs),
+      ymin: Math.min(ymin, ...triangleYs),
+      xmax: Math.max(xmax, ...triangleXs),
+      ymax: Math.max(ymax, ...triangleYs)
+    };
+  }, {
+    xmin: Number.POSITIVE_INFINITY,
+    ymin: Number.POSITIVE_INFINITY,
+    xmax: Number.NEGATIVE_INFINITY,
+    ymax: Number.NEGATIVE_INFINITY
+  });
+}
+
+const path = tag("path");
+const animate = tag("animate");
+const svg = tag("svg").bind(this, {
+  id:"gem",
+  xmlns:"http://www.w3.org/2000/svg",
+  "xmlns:xlink":"http://www.w3.org/1999/xlink",
+  width:"500px",
+  height:"500px",
+
+  ...(({xmin, xmax, ymin, ymax}, width = xmax-xmin, height = ymax-ymin) => ({
+    viewBox: `${xmin} ${ymin} ${width} ${height}`
+  }))(getDimensions(triangles))
+});
+
 function triangle(one, two, three, ...colors) {
   return path({
-    d: `M${one}L${two}L${three}z`
+    d: `M${one.x},${one.y}L${two.x},${two.y}L${three.x},${three.y}z`
   }, animateColors(colors));
-}
-
-function triangle1(one, ...colors) {
-  return path({
-    d: `M${one}l1 2l1 -2z`
-  }, animateColors(colors));
-}
-
-function mesh(num, width, height) {
-  const verticies = [],
-        area = Math.sqrt(width * height / num / 2);
-  console.log(area);
-
-  for (let i=0, row=1; i<num; row++) {
-    for (let offset=0; offset<width; i++, offset+=area) {
-      const x = offset + Math.random() * area,
-            y = row * Math.random() * area,
-            v = `${x} ${y}`;
-      console.log(v);
-      verticies[i] = [x, y];
-    }
-  }
-  return verticies;
 }
 
 const svgStr = svg(
-  mesh(10, 300, 200).map(v => triangle1(`${v[0]} ${v[1]}`, "black"))
+  triangles.map(({one, two, three, color}) => triangle(one, two, three, color))
 );
 
 fs.writeFile("www/gems.svg", svgStr, err => {
