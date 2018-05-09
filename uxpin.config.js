0b8f09058647620ec23fce3e0be284b9c4351352
Michał Stocki <michal.stocki@uxpin.com>
2018-05-09 14:36:56 +0200

    chore(uxpin): updated uxpin config include all components from v0.32.0

--- uxpin.config.js
+++ uxpin.config.js
@@ -2,21 +2,52 @@ module.exports = {
   components: {
     categories: [
       {
-        name: 'Uncategorized',
+        name: 'General',
         include: [
+          'src/library/Avatar/Avatar.js',
           'src/library/Button/Button.js',
-          'src/library/Card/Card.js',
+          'src/library/Checkbox/Checkbox.js',
+          'src/library/Checkbox/CheckboxGroup.js',
+          'src/library/Choice/Choice.js',
+          'src/library/Choice/ChoiceGroup.js',
           'src/library/Dropdown/Dropdown.js',
+          'src/library/Form/FormField.js',
+          'src/library/Form/FormFieldDivider.js',
+          'src/library/Form/FormFieldset.js',
           'src/library/Link/Link.js',
           'src/library/Menu/Menu.js',
+          'src/library/Menu/MenuDivider.js',
+          'src/library/Menu/MenuItem.js',
+          'src/library/Menu/MenuGroup.js',
           'src/library/Popover/Popover.js',
-          'src/library/Portal/Portal.js',
+          'src/library/Radio/Radio.js',
+          'src/library/Radio/RadioGroup.js',
+          'src/library/Select/Select.js',
+          'src/library/Text/Text.js',
+          'src/library/TextArea/TextArea.js',
           'src/library/TextInput/TextInput.js',
+          'src/library/themes/ThemeProvider.js',
+          'src/library/Tooltip/Tooltip.js',
         ]
+      },
+      {
+        name: 'Card',
+        include: 'src/library/Card/!(index|CardRow).js'
       },
       {
         name: 'Icons',
         include: 'src/library/Icon/!(index).js'
+      },
+      {
+        name: 'Layout',
+        include: [
+          'src/library/Box/Box.js',
+          'src/library/Flex/Flex.js',
+          'src/library/Flex/FlexItem.js',
+          'src/library/Grid/Grid.js',
+          'src/library/Grid/GridItem.js',
+          'src/library/StartEnd/StartEnd.js',
+        ]
       }
     ]
   }
