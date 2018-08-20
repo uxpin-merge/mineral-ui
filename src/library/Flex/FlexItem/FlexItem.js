88504a208837ff052a3073bb994d8bf4248f1efb
Michał Stocki <michal.stocki@uxpin.com>
2018-08-20 16:35:40 +0200

    chore(uxpin): Copied all props from Box to FlexItem and GridItem

--- src/library/Flex/FlexItem/FlexItem.js
+++ src/library/Flex/FlexItem/FlexItem.js
@@ -26,6 +26,427 @@ type Props = {
    * [[Responsive-capable]](#responsive)
    */
   shrink?: number | Array<number | null>,
+  /** Rendered root HTML element */
+  element?: string,
+  /** Sets the box height. [[Responsive-capable]](#responsive) */
+  height?: number | string | Array<number | string | null>,
+  /** Renders Box as an inline-block [[Responsive-capable]](#responsive) */
+  inline?: boolean | Array<boolean | null>,
+  /** Applies a margin on all sides [[Responsive-capable]](#responsive) */
+  margin?:
+    | 'xxs'
+    | 'xs'
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | 'xl'
+    | 'xxl'
+    | number
+    | string
+    | Array<
+      | 'xxs'
+      | 'xs'
+      | 'sm'
+      | 'md'
+      | 'lg'
+      | 'xl'
+      | 'xxl'
+      | number
+      | string
+      | null
+    >,
+  /** Applies a bottom margin [[Responsive-capable]](#responsive) */
+  marginBottom?:
+    | 'xxs'
+    | 'xs'
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | 'xl'
+    | 'xxl'
+    | number
+    | string
+    | Array<
+      | 'xxs'
+      | 'xs'
+      | 'sm'
+      | 'md'
+      | 'lg'
+      | 'xl'
+      | 'xxl'
+      | number
+      | string
+      | null
+    >,
+  /**
+   * Applies a right margin when the language is left-to-right and left margin
+   * [for RTL languages](#rtl) [[Responsive-capable]](#responsive)
+   */
+  marginEnd?:
+    | 'xxs'
+    | 'xs'
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | 'xl'
+    | 'xxl'
+    | number
+    | string
+    | Array<
+      | 'xxs'
+      | 'xs'
+      | 'sm'
+      | 'md'
+      | 'lg'
+      | 'xl'
+      | 'xxl'
+      | number
+      | string
+      | null
+    >,
+  /** Applies left & right margins [[Responsive-capable]](#responsive) */
+  marginHorizontal?:
+    | 'xxs'
+    | 'xs'
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | 'xl'
+    | 'xxl'
+    | number
+    | string
+    | Array<
+      | 'xxs'
+      | 'xs'
+      | 'sm'
+      | 'md'
+      | 'lg'
+      | 'xl'
+      | 'xxl'
+      | number
+      | string
+      | null
+    >,
+  /** Applies a left margin [[Responsive-capable]](#responsive) */
+  marginLeft?:
+    | 'xxs'
+    | 'xs'
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | 'xl'
+    | 'xxl'
+    | number
+    | string
+    | Array<
+      | 'xxs'
+      | 'xs'
+      | 'sm'
+      | 'md'
+      | 'lg'
+      | 'xl'
+      | 'xxl'
+      | number
+      | string
+      | null
+    >,
+  /** Applies a right margin [[Responsive-capable]](#responsive) */
+  marginRight?:
+    | 'xxs'
+    | 'xs'
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | 'xl'
+    | 'xxl'
+    | number
+    | string
+    | Array<
+      | 'xxs'
+      | 'xs'
+      | 'sm'
+      | 'md'
+      | 'lg'
+      | 'xl'
+      | 'xxl'
+      | number
+      | string
+      | null
+    >,
+  /**
+   * Applies a left margin when the language is left-to-right and right margin
+   * [for RTL languages](#rtl) [[Responsive-capable]](#responsive)
+   */
+  marginStart?:
+    | 'xxs'
+    | 'xs'
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | 'xl'
+    | 'xxl'
+    | number
+    | string
+    | Array<
+      | 'xxs'
+      | 'xs'
+      | 'sm'
+      | 'md'
+      | 'lg'
+      | 'xl'
+      | 'xxl'
+      | number
+      | string
+      | null
+    >,
+  /** Applies a top margin [[Responsive-capable]](#responsive) */
+  marginTop?:
+    | 'xxs'
+    | 'xs'
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | 'xl'
+    | 'xxl'
+    | number
+    | string
+    | Array<
+      | 'xxs'
+      | 'xs'
+      | 'sm'
+      | 'md'
+      | 'lg'
+      | 'xl'
+      | 'xxl'
+      | number
+      | string
+      | null
+    >,
+  /**
+   * Applies top & bottom margins [[Responsive-capable]](#responsive)
+   */
+  marginVertical?:
+    | 'xxs'
+    | 'xs'
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | 'xl'
+    | 'xxl'
+    | number
+    | string
+    | Array<
+      | 'xxs'
+      | 'xs'
+      | 'sm'
+      | 'md'
+      | 'lg'
+      | 'xl'
+      | 'xxl'
+      | number
+      | string
+      | null
+    >,
+  /** Applies padding to all sides [[Responsive-capable]](#responsive) */
+  padding?:
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | number
+    | string
+    | Array<'sm' | 'md' | 'lg' | number | string | null>,
+  /** Applies bottom padding [[Responsive-capable]](#responsive) */
+  paddingBottom?:
+    | 'xxs'
+    | 'xs'
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | 'xl'
+    | 'xxl'
+    | number
+    | string
+    | Array<
+      | 'xxs'
+      | 'xs'
+      | 'sm'
+      | 'md'
+      | 'lg'
+      | 'xl'
+      | 'xxl'
+      | number
+      | string
+      | null
+    >,
+  /**
+   * Applies right padding when the language is left-to-right and left padding
+   * [for RTL languages](#rtl) [[Responsive-capable]](#responsive)
+   */
+  padddingEnd?:
+    | 'xxs'
+    | 'xs'
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | 'xl'
+    | 'xxl'
+    | number
+    | string
+    | Array<
+      | 'xxs'
+      | 'xs'
+      | 'sm'
+      | 'md'
+      | 'lg'
+      | 'xl'
+      | 'xxl'
+      | number
+      | string
+      | null
+    >,
+  /** Applies left & right padding [[Responsive-capable]](#responsive) */
+  paddingHorizontal?:
+    | 'xxs'
+    | 'xs'
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | 'xl'
+    | 'xxl'
+    | number
+    | string
+    | Array<
+      | 'xxs'
+      | 'xs'
+      | 'sm'
+      | 'md'
+      | 'lg'
+      | 'xl'
+      | 'xxl'
+      | number
+      | string
+      | null
+    >,
+  /** Applies left padding [[Responsive-capable]](#responsive) */
+  paddingLeft?:
+    | 'xxs'
+    | 'xs'
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | 'xl'
+    | 'xxl'
+    | number
+    | string
+    | Array<
+      | 'xxs'
+      | 'xs'
+      | 'sm'
+      | 'md'
+      | 'lg'
+      | 'xl'
+      | 'xxl'
+      | number
+      | string
+      | null
+    >,
+  /** Applies right padding [[Responsive-capable]](#responsive) */
+  paddingRight?:
+    | 'xxs'
+    | 'xs'
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | 'xl'
+    | 'xxl'
+    | number
+    | string
+    | Array<
+      | 'xxs'
+      | 'xs'
+      | 'sm'
+      | 'md'
+      | 'lg'
+      | 'xl'
+      | 'xxl'
+      | number
+      | string
+      | null
+    >,
+  /**
+   * Applies left padding when the language is left-to-right and right padding
+   * [for RTL languages](#rtl) [[Responsive-capable]](#responsive)
+   */
+  padddingStart?:
+    | 'xxs'
+    | 'xs'
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | 'xl'
+    | 'xxl'
+    | number
+    | string
+    | Array<
+      | 'xxs'
+      | 'xs'
+      | 'sm'
+      | 'md'
+      | 'lg'
+      | 'xl'
+      | 'xxl'
+      | number
+      | string
+      | null
+    >,
+  /** Applies bottom padding [[Responsive-capable]](#responsive) */
+  paddingTop?:
+    | 'xxs'
+    | 'xs'
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | 'xl'
+    | 'xxl'
+    | number
+    | string
+    | Array<
+      | 'xxs'
+      | 'xs'
+      | 'sm'
+      | 'md'
+      | 'lg'
+      | 'xl'
+      | 'xxl'
+      | number
+      | string
+      | null
+    >,
+  /** Applies top & bottom margins [[Responsive-capable]](#responsive) */
+  paddingVertical?:
+    | 'xxs'
+    | 'xs'
+    | 'sm'
+    | 'md'
+    | 'lg'
+    | 'xl'
+    | 'xxl'
+    | number
+    | string
+    | Array<
+      | 'xxs'
+      | 'xs'
+      | 'sm'
+      | 'md'
+      | 'lg'
+      | 'xl'
+      | 'xxl'
+      | number
+      | string
+      | null
+    >,
+  /** Sets the box width [[Responsive-capable]](#responsive) */
+  width?: number | string | Array<number | string | null>,
   children: React$Node
 };
 
